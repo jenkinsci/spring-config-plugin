07eedaa5a50f5aa5fd23447b24ce250ee2801859
Gong Yi <topikachu@163.com>
2020-06-26 17:13:04 +0800

    Implement remote execution

--- src/test/java/org/jenkinsci/plugins/springconfig/SpringConfigTest.java
+++ src/test/java/org/jenkinsci/plugins/springconfig/SpringConfigTest.java
@@ -1,11 +1,14 @@
 package org.jenkinsci.plugins.springconfig;
 
+import hudson.FilePath;
+import jenkins.model.Jenkins;
 import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -21,17 +24,16 @@ public class SpringConfigTest {
 	public JenkinsRule r = new JenkinsRule();
 
 	@Test
-	public void testReadSpringConfigWithoutProfile() throws Exception {
-		WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "p");
-		p.setDefinition(new CpsFlowDefinition("print springConfig().getProperty('foo')", true));
-		WorkflowRun b = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
-		r.assertLogContains("bar", b);
-	}
-
-	@Test
 	public void testReadSpringConfigWithProfile() throws Exception {
-		WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "p");
-		p.setDefinition(new CpsFlowDefinition("print springConfig(profiles: 'bar').getProperty('foo')", true));
+		Jenkins jenkins = r.jenkins;
+		WorkflowJob p = jenkins.createProject(WorkflowJob.class, "p");
+		FilePath ws = jenkins.getWorkspaceFor(p);
+		FilePath applicationYaml = ws.child("application.yaml");
+		applicationYaml.copyFrom(this.getClass().getClassLoader().getResourceAsStream("nodefault/application.yaml"));
+		FilePath applicationBarYaml = ws.child("application-bar.yaml");
+		applicationBarYaml
+				.copyFrom(this.getClass().getClassLoader().getResourceAsStream("nodefault/application-bar.yaml"));
+		p.setDefinition(new CpsFlowDefinition("node {print springConfig(profiles: ['bar']).properties.foo}", true));
 		WorkflowRun b = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
 		r.assertLogContains("barporfile", b);
 	}
@@ -39,25 +41,25 @@ public class SpringConfigTest {
 	@Test
 	@SneakyThrows
 	public void testReadSpringConfigWithLocationAndProfile() throws Exception {
-		File gooProfile = new File("custom-config/application-goo.yaml");
-		FileUtils.forceMkdirParent(gooProfile);
-		FileUtils.touch(gooProfile);
-		try (InputStream gooInputStream = SpringConfigTest.class.getClassLoader().getResourceAsStream("goo.yaml");
-				OutputStream gooOutputStream = new FileOutputStream(gooProfile)) {
-			IOUtils.copy(gooInputStream, gooOutputStream);
-		}
-		WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "p");
+		Jenkins jenkins = r.jenkins;
+		WorkflowJob p = jenkins.createProject(WorkflowJob.class, "p");
+		FilePath applicationGooYaml = jenkins.getWorkspaceFor(p).child("custom-config").child("application-goo.yaml");
+		applicationGooYaml
+				.copyFrom(this.getClass().getClassLoader().getResourceAsStream("nodefault/application-goo.yaml"));
 		p.setDefinition(new CpsFlowDefinition(
-				"print springConfig(profiles: 'goo', location : 'custom-config/').getProperty('foo')", true));
+				"node{print springConfig(profiles: ['goo'], location : 'custom-config/').properties.foo}", true));
 		WorkflowRun b = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
 		r.assertLogContains("gooporfile", b);
 	}
 
 	@Test
 	public void testReadSpringConfigAsProperties() throws Exception {
-		WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "p");
-		p.setDefinition(new CpsFlowDefinition("" + "def config=springConfig() \n" + "print config.properties.foo \n"
-				+ "print config.properties['a.b.c']", true));
+		Jenkins jenkins = r.jenkins;
+		WorkflowJob p = jenkins.createProject(WorkflowJob.class, "p");
+		FilePath applicationYaml = jenkins.getWorkspaceFor(p).child("application.yaml");
+		applicationYaml.copyFrom(this.getClass().getClassLoader().getResourceAsStream("nodefault/application.yaml"));
+		p.setDefinition(new CpsFlowDefinition("node {" + "def config=springConfig() \n"
+				+ "print config.properties.foo \n" + "print config.properties['a.b.c']}", true));
 		WorkflowRun b = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
 		r.assertLogContains("bar", b);
 		r.assertLogContains("nestedvalue", b);
@@ -65,8 +67,11 @@ public class SpringConfigTest {
 
 	@Test
 	public void testReadSpringConfigAsNestedMap() throws Exception {
-		WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "p");
-		p.setDefinition(new CpsFlowDefinition("print springConfig().nestedMap.a.b.c", true));
+		Jenkins jenkins = r.jenkins;
+		WorkflowJob p = jenkins.createProject(WorkflowJob.class, "p");
+		FilePath applicationYaml = jenkins.getWorkspaceFor(p).child("application.yaml");
+		applicationYaml.copyFrom(this.getClass().getClassLoader().getResourceAsStream("nodefault/application.yaml"));
+		p.setDefinition(new CpsFlowDefinition("node {print springConfig().nestedMap.a.b.c}", true));
 		WorkflowRun b = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
 		r.assertLogContains("nestedvalue", b);
 	}
